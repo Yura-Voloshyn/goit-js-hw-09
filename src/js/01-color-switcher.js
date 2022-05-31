734b6773f299dd6b25a83687d7c703603aafc649
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-05-31 12:47:47 +0300

    f

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let timerId = null;
+
+refs.startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    console.log('lets party started');
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(`take a brake`);
+  refs.stopBtn.disabled = true;
+  refs.startBtn.disabled = false;
+});
